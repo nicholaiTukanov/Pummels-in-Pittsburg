b9f65428c0f16188d10065f4dd28b9dedfc5ade3
Christopher Frantz <cfrantz@cs.utexas.edu>
2019-11-21 11:24:28 -0600

    Implement imputations for missing values

--- predict.py
+++ predict.py
@@ -8,26 +8,26 @@ from sklearn.preprocessing import Imputer
 
 
 def predict_severity(df):
-    features, labels = df.drop(columns=['MAX_SEVERITY_LEVEL']), df.MAX_SEVERITY_LEVEL
+    features, labels = df.drop(
+        columns=['MAX_SEVERITY_LEVEL']), df.MAX_SEVERITY_LEVEL
+
     features_train, features_test, labels_train, labels_test = \
         sk.model_selection.train_test_split(features, labels, test_size=0.2)
 
-    nan_columns = df.columns[df.isna().any()].tolist()
     numeric_transformer = Pipeline(steps=[
-        ("imputer", Imputer(missing_values=0, strategy="median", axis=0))])
+        ("imputer", Imputer(missing_values='NaN', strategy="median", axis=0))])
 
     clf = Pipeline(steps=[('preprocessor', numeric_transformer),
-                ('classifier', tree.DecisionTreeClassifier(criterion='entropy'))])
+                          ('classifier', tree.DecisionTreeClassifier(criterion='entropy'))])
 
-    clf.fit(features_train,labels_train)
+    clf.fit(features_train, labels_train)
 
-    features_test = numeric_transformer.fit(features_test)
-
-    print(features_test)
+    features_test = numeric_transformer.fit_transform(features_test)
 
     prediction = clf.predict(features_test)
     acc = sk.metrics.accuracy_score(prediction, labels_test)
     print('Accuracy', acc)
+
 
 def main():
     df = clean.get_clean_data()
